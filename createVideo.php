57badc21e675ae4f2694a34ab24075ba8b4573dc
thomashanahan <thmsshanahan0815@gmail.com>
2018-12-17 01:18:21 -0800

    rst delete cancel

--- createVideo.php
+++ createVideo.php
@@ -3,7 +3,7 @@
 # Input data
 $audio_file = "src/audio.mp3";
 $slide_duration = 5;
-$result_file = "newvideo.mp4";
+$result_file_name = "newvideo";
 $data = [
   [
     "image" => "src/images/image1.jpg",
@@ -27,27 +27,30 @@ $data = [
   ],
 ];
 
-createVideo($data, $slide_duration, $audio_file, $result_file);
+createVideo($data, $slide_duration, $audio_file, $result_file_name);
 
-function createVideo($data, $slide_duration, $audio_file, $result_file) {
+function createVideo($data, $slide_duration, $audio_file, $result_file_name) {
   
-  $width = 460;
-  $height = 320;
-  $scale = get_scale($width, $height);
-  $video_coder = "-c:v libx264";
-  $temp_path = "temp/";
-  $merge_file = "temp/input.txt";
-  $subtitle = "temp/subtitles.srt";
-  $image_count = sizeof($data);
-  $total_time = "-t ".$slide_duration * $image_count;
+  $width          = 460;
+  $height         = 320;
+  $scale	        = get_scale($width, $height);
+  $video_coder	  = "-c:v libx264";
+  $temp_path	    = "temp/";
+  $merge_file     = "temp/input.txt";
+  $subtitle       = $result_file_name.".srt";
+  $result_file    = $result_file_name.".mp4";
+  $image_count    = sizeof($data);
+  $total_time     = "-t ".$slide_duration * $image_count;
 
   init_data($data, $slide_duration, $merge_file, $subtitle);
 
   foreach($data as $key => $row) {
+
     convert_image_video($row['image'], $slide_duration, $key, $video_coder, $temp_path, $scale);
     $result_image_path = $temp_path.$key.".mp4";
     add_slide_video($result_image_path, $slide_duration, $key);
     add_merge_file($key, $merge_file);
+
   }
 
   merge_videos($merge_file);
@@ -55,37 +58,44 @@ function createVideo($data, $slide_duration, $audio_file, $result_file) {
   add_subtitle_video($subtitle, $result_file);
   delete_temp_files();
   rmdir("temp");
+
 }
 
 function init_data($data, $slide_duration, $merge_file_path, $subtitle){
+
   mkdir("temp");
   delete_temp_files();
   create_subtitle($data, $slide_duration, $subtitle);
 }
 
 function delete_temp_files(){
+
   $files = glob('temp/*'); 
   foreach($files as $file){ 
     if(is_file($file))
        unlink($file);
-    }
+  }
 }
 
 function create_subtitle($data, $slide_duration, $subtitle){
+
   $file = fopen($subtitle, "w");
+
   foreach($data as $key => $row) {
     $txt = ($key + 1)."\n";
     $txt .= create_duration($key, $slide_duration);
     $txt .= "\n";
     $txt .= $row["subtitle"];
     $txt .= "\n\n";
-    create_duration($key, $slide_duration);
+
     fwrite($file, $txt);
   }
+
   fclose($file);
 }
 
 function create_duration($key, $slide_duration){
+
   $start_time = format_seconds($key * $slide_duration);
   $end_time = format_seconds(($key + 1) * $slide_duration);
   return "$start_time,00 --> $end_time,00";
@@ -109,36 +119,42 @@ function format_seconds( $seconds )
 }
 
 function add_merge_file($key, $merge_file){
+
   $file = fopen($merge_file, "a");
   $txt = "file '$key.mp4' \n";
   fwrite($file, $txt);
   fclose($file);
-}
+  }
 
 function convert_image_video($input_image, $slide_duration, $key, $video_coder, $temp_path, $scale) {
+
   $ffmpeg = "ffmpeg -loop 1 -i $input_image $video_coder -t $slide_duration -r 30 -pix_fmt yuv420p -y -vf $scale $temp_path$key.mp4";
   shell_exec($ffmpeg);
 }
 
 function add_slide_video($input_image, $slide_duration, $key) {
+
   $add_fade_in ="ffmpeg -i " .$input_image. " -strict experimental -y -vf fade=in:0:30 temp/temp.mp4";
   shell_exec($add_fade_in);
+
   $add_fade_out ="ffmpeg -i temp/temp.mp4 -strict experimental -y -vf fade=out:".(($slide_duration-1)*30).":30 ".$input_image;
   shell_exec($add_fade_out);
-  
 }
   
 function merge_videos($merge_file){
+
   $cat = "ffmpeg -f concat -i $merge_file -y -codec copy temp/temp.mp4";
   shell_exec($cat);
 }
 
 function add_audio_video($total_time, $audio_file, $video_coder, $scale) {
+
   $ffmpeg = "ffmpeg -y -i temp/temp.mp4 $total_time -i $audio_file $video_coder -r 30 -c:a aac -vf $scale temp/temp1.mp4";
   shell_exec($ffmpeg);
 }
 
 function add_subtitle_video($subtitle, $result_file) {
+
   $slide_ffmpeg ="ffmpeg -y -i temp/temp1.mp4 -f srt -i $subtitle -c:v copy -c:a copy -c:s mov_text $result_file";
   shell_exec($slide_ffmpeg);
 }
@@ -146,4 +162,5 @@ function add_subtitle_video($subtitle, $result_file) {
 function get_scale($width, $height){
   return "scale=".$width.":".$height;
 }
+
 ?>
